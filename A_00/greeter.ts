70ec7f2f27682fdf35b713fb5653909f7bb1345b
Nadine Limberger <Nadine Limberger@141.28.132.165>
2017-03-20 10:37:29 +0100

    a

--- A_00/greeter.ts
+++ A_00/greeter.ts
@@ -4,8 +4,8 @@ Matrikel: 255438
 Datum: 20.03.2017
 Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde nicht kopiert und auch nicht diktiert.*/
 
-document.addEventListener('DOMContentLoaded', function () {
-    var name = prompt("Wie heißt du? :-)");
-    var greeting = "Ich wünsche dir einen schönen Tag ";
+document.addEventListener("DOMContentLoaded", function (): void {
+    var name: string = prompt("Wie heißt du? :-)");
+    var greeting: string = "Ich wünsche dir einen schönen Tag ";
     document.body.innerHTML = greeting + name;
 });
