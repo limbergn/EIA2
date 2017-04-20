76a598578554893a6d13e6776e3d0492d13a3be3
Nadine Limberger <Nadine Limberger@141.28.129.86>
2017-04-20 12:09:43 +0200

    aa

--- A_03/maumau.js
+++ A_03/maumau.js
@@ -3,14 +3,79 @@
 //Matrikelnr.: 255346
 //Datum: 30.03.2017
 //Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde nicht kopiert und auch nicht diktiert. 
-let karten = ["Karo7", "Karo8", "Karo9", "Karo10", "KaroBube", "KaroDame", "KaroKöing",
-    "Herz7", "Herz8", "Herz9", "Herz10", "HerzBube", "HerzDame", "HerzKönig",
-    "Kreuz7", "Kreuz8", "Kreuz9", "Kreuz10", "KreuzBube", "KreuzDame", "KreuzKönig",
-    "Pik7", "Pik8", "Pik9", "Pik10", "PikBube", "PikDame", "PikKönig"];
-let gezogen = 0;
-let abgelegt = 0;
-let rest;
-//let rest: number = 32 - gezogen - abgelegt
-document.getElementById("ziehstapel").addEventListener("click", function () {
+//Habe den Code mit Unterstüzung von meinem Freund erstellt. 
+//Habe noch große Verständnisprobleme
+document.addEventListener("DOMContentLoaded", function () {
+    var cards = ["Karo 7",
+        "Karo 8",
+        "Karo 9",
+        "Karo 10",
+        "Karo Bube",
+        "Karo Dame",
+        "Karo König",
+        "Karo As",
+        "Kreuz 7",
+        "Kreuz 8",
+        "Kreuz 9",
+        "Kreuz 10",
+        "Kreuz Bube",
+        "Kreuz Dame",
+        "Kreuz König",
+        "Kreuz As",
+        "Herz 7",
+        "Herz 8",
+        "Herz 9",
+        "Herz 10",
+        "Herz Bube",
+        "Herz Dame",
+        "Herz König",
+        "Herz As",
+        "Pik 7",
+        "Pik 8",
+        "Pik 9",
+        "Pik 10",
+        "Pik Bube",
+        "Pik Dame",
+        "Pik König",
+        "Pik As"
+    ];
+    /*Click on Deck("Take") --> max 5 Karten auf der Hand */
+    let take = document.getElementById("take");
+    take.addEventListener("click", function () {
+        /*Place Div/Card - zufällige Karte aus "placeCardToHand" nur wenn "Hand" > 4*/
+        let cardsOnHand = document.getElementById("hand").childElementCount;
+        if (cardsOnHand < 5) {
+            placeCardToHand();
+        }
+    });
+    /* Funktion zur Platzierung der Karten-Divs im Hand-Div */
+    function placeCardToHand() {
+        let div = document.createElement("div");
+        document.getElementById("hand").appendChild(div);
+        let s = div.style;
+        /* Zufällige Karte */
+        let n = Math.round(Math.random() * cards.length);
+        while (cards[n] == undefined) {
+            Math.round(Math.random() * cards.length);
+        }
+        div.innerText = cards[n];
+        s.border = "solid black";
+        s.display = "inline-block";
+        s.width = "200px";
+        s.height = "250px";
+        div.className = "cardsDrawn";
+        console.log(cards[n]);
+        console.log(cards.length);
+        cards.splice(n, 1);
+        /* Aktivität 2: Drop card from "Hand" to "Drop"-Deck - Anzahl der "Drop" Karten anzeigen */
+        div.addEventListener("click", function () {
+            let cardsDropped = [""];
+            let cardValue = div.innerText;
+            let amountOfDroppedCards = cardsDropped.push(cardValue);
+            document.getElementById("drop").innerText = cardValue;
+            /* Handkarte löschen*/
+            div.parentNode.removeChild(div);
+        });
+    }
 });
 //# sourceMappingURL=maumau.js.map
--- A_03/maumau.ts
+++ A_03/maumau.ts
@@ -7,53 +7,82 @@
 //Habe den Code mit Unterstüzung von meinem Freund erstellt. 
 //Habe noch große Verständnisprobleme
 
-document.addEventListener("DOMContentLoaded", function() {
-
-    let kartenSammlung = ["Pik 7", "Pik 8", "Pik 9",
-        "Pik 10", "Pik Bube", "Pik Dame",
-        "Pik König", "Pik As",
-        "Kreuz 7", "Kreuz 8", "Kreuz 9",
-        "Kreuz 10", "Kreuz Bube", "Kreuz Dame",
-        "Kreuz König", "Kreuz As",
-        "Karo 7", "Karo 8", "Karo 9",
-        "Karo 10", "Karo Bube", "Karo Dame",
-        "Karo König", "Karo As",
-        "Herz 7", "Herz 8", "Herz 9",
-        "Herz 10", "Herz Bube", "Herz Dame",
-        "Herz König", "Herz As"];
-
-    let nachziehStapel = document.getElementById("deck");
-
-    nachziehStapel.addEventListener("click", function() {
-
-        let zufallsKarte = kartenSammlung.splice(Math.round(Math.random() * (kartenSammlung.length - 1)), 1)[0];
-
-        let leerKarte = sucheLeerKarte();
-        function sucheLeerKarte() {
-            for (let i = 0; i < handKarten.length; i++) {
-                if (handKarten[i].textContent === "") {
-                    return handKarten[i];
-                }
-            }
-            return null;
-        }
-        if (leerKarte != null) {
-            leerKarte.innerText = zufallsKarte;
-        }
-        if (kartenSammlung.length === 0) {
-            nachziehStapel.innerText = "leer";
-            nachziehStapel.style.display = "none";
+document.addEventListener("DOMContentLoaded", function () {
+    var cards = ["Karo 7",
+        "Karo 8",
+        "Karo 9",
+        "Karo 10",
+        "Karo Bube",
+        "Karo Dame",
+        "Karo König",
+        "Karo As",
+        "Kreuz 7",
+        "Kreuz 8",
+        "Kreuz 9",
+        "Kreuz 10",
+        "Kreuz Bube",
+        "Kreuz Dame",
+        "Kreuz König",
+        "Kreuz As",
+        "Herz 7",
+        "Herz 8",
+        "Herz 9",
+        "Herz 10",
+        "Herz Bube",
+        "Herz Dame",
+        "Herz König",
+        "Herz As",
+        "Pik 7",
+        "Pik 8",
+        "Pik 9",
+        "Pik 10",
+        "Pik Bube",
+        "Pik Dame",
+        "Pik König",
+        "Pik As"
+    ];
+    /*Click on Deck("Take") --> max 5 Karten auf der Hand */
+    let take = document.getElementById("take");
+    
+    take.addEventListener("click", function () {
+        /*Place Div/Card - zufällige Karte aus "placeCardToHand" nur wenn "Hand" > 4*/
+        let cardsOnHand = document.getElementById("hand").childElementCount;
+        if (cardsOnHand < 5) {
+            placeCardToHand();
         }
     });
-    // ABLAGE 
-    let ablage = document.getElementById("discard");
-    let handKarten = document.getElementsByClassName("Handkarten");
-    for (let v = 0; v < handKarten.length; v++) {
-        //Karten von Hand in die Ablage
-        handKarten[v].addEventListener("click", function(event) {
-            let karteHand = event.target;
-            ablage.textContent = karteHand.textContent;
-            karteHand.textContent = "";
+    
+    
+    /* Funktion zur Platzierung der Karten-Divs im Hand-Div */
+    function placeCardToHand() {
+        let div = document.createElement("div");
+        document.getElementById("hand").appendChild(div);
+        let s = div.style;
+        /* Zufällige Karte */
+        let n = Math.round(Math.random() * cards.length);
+        while (cards[n] == undefined) {
+            Math.round(Math.random() * cards.length);
+        }
+        
+        
+        div.innerText = cards[n];
+        s.border = "solid black";
+        s.display = "inline-block";
+        s.width = "200px";
+        s.height = "250px";
+        div.className = "cardsDrawn";
+        console.log(cards[n]);
+        console.log(cards.length);
+        cards.splice(n, 1);
+        
+        /* Aktivität 2: Drop card from "Hand" to "Drop"-Deck - Anzahl der "Drop" Karten anzeigen */
+        div.addEventListener("click", function () {
+            let cardsDropped = [""];
+            let cardValue = div.innerText;
+            let amountOfDroppedCards = cardsDropped.push(cardValue);
+            document.getElementById("drop").innerText = cardValue;
+            /* Handkarte löschen*/
+            div.parentNode.removeChild(div);
         });
     }
 });
