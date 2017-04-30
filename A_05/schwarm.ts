af8ccc11ae49dd50188e0e8b8d36bd0ec905fb49
Nadine Limberger <Nadine Limberger@LAPTOP-NL.fritz.box>
2017-04-30 17:21:39 +0200

    aa

--- A_05/schwarm.ts
+++ A_05/schwarm.ts
@@ -4,20 +4,21 @@
 //  Datum: 13.04.2017
 //  Hiermit versichere ich, dass ich diesen Code selbst geschrieben habe. Er wurde nicht kopiert und auch nicht diktiert. 
 
-namespace Aufgabe5 {
+namespace A05_Bienenschwarm {
     window.addEventListener("load", init);
     let crc2: CanvasRenderingContext2D;
 
     let x: number[] = [];
     let y: number[] = [];
-    let count: number = 10;
     let saveBackground: ImageData;
+    let count: number = 10;
 
 
     function init(_event: Event): void {
         let canvas: HTMLCanvasElement;
         canvas = document.getElementsByTagName("canvas")[0];
         crc2 = canvas.getContext("2d");
+
         //Wiese
         crc2.beginPath();
         crc2.fillStyle = "#32Cd32";
@@ -118,8 +119,9 @@ namespace Aufgabe5 {
             crc2.fill();
             crc2.stroke();
         }
-        
-        drawbeehive(550,850, "#8A4B08","#000000");
+
+
+        drawBienenstock(550, 810, "#DEB887", "#8B4513");
         drawFlower(900, 900);
         drawFlower(400, 700);
         drawFlower(100, 800);
@@ -133,31 +135,25 @@ namespace Aufgabe5 {
         drawBlume(1750, 1000, "#008000", "#FE9A2E");
         drawBlume(1300, 800, "#008000", "#FE9A2E");
 
-
-
-
-        //Hintergrund speichern
         saveBackground = crc2.getImageData(0, 0, 1920, 1080);
 
-        //Anfangsposition der Bienen
+
+        //Animation Bienen
         for (let i: number = 0; i < count; i++) {
             x[i] = 558;
             y[i] = 810;
         }
 
-        //Animation der Bienen
         function animate(): void {
-            //Hintergrungbild das gespeichert wurde, wird aufgerufen
             crc2.putImageData(saveBackground, 0, 0);
 
             for (let i: number = 0; i < count; i++) {
                 x[i] += Math.random() * 1 - 2;
                 y[i] += Math.random() * 4 - 2;
 
-                //Malen der Bienen an der neuen Position
-                drawBee(x[i], y[i], "#FFFF00", "#000000");
+                drawBiene(x[i], y[i], "#FFFF00", "#000000");
 
-                //Bienen erscheinen am gegenüberliegen Rand
+                //WENN Bienen Canvas verlassen, DANN starten sie von gegenüber
                 if (x[i] >= 1920)
                     x[i] = 0;
 
@@ -174,18 +170,18 @@ namespace Aufgabe5 {
         }
 
         //Anfangsposition der neuen Biene
-        function newBee(_event: Event): void {
+        function neueBiene(_event: Event): void {
             x.push(558);
             y.push(820);
             count++;
         }
+
+        //Click-/Touchevent
         window.setTimeout(animate, 22);
-        canvas.addEventListener("click", newBee); //Bei Click auf den Canvas erscheint eine neue Biene
-        canvas.addEventListener("touch", newBee); //Bei Touch auf den Canvas erscheint eine neue Biene
+        canvas.addEventListener("click", neueBiene);
+        canvas.addEventListener("touch", neueBiene);
 
         // Blumen
-
-
         function drawBlume(_x: number, _y: number, _strokeColor: string, _fillColor: string): void {
 
             crc2.beginPath();
@@ -198,7 +194,7 @@ namespace Aufgabe5 {
             crc2.lineTo(_x - 10, _y - 30);
             crc2.closePath();
             crc2.stroke();
-            //Bl�te
+            //Blüte
             crc2.beginPath();
             crc2.arc(_x, _y - 50, 14, 0 * Math.PI, 1 * Math.PI);
             crc2.lineTo(_x - 10, _y - 60);
@@ -210,7 +206,6 @@ namespace Aufgabe5 {
             crc2.fillStyle = _fillColor;
             crc2.fill();
         }
-
 
         function drawTulip(_x: number, _y: number, _color: string): void {
             //Stiel
@@ -236,7 +231,6 @@ namespace Aufgabe5 {
             crc2.fill();
             crc2.closePath();
         }
-
 
         function drawFlower(_x: number, _y: number): void {
             //Blütenblätter
@@ -279,54 +273,94 @@ namespace Aufgabe5 {
             crc2.stroke();
         }
 
-
-        function drawbeehive(_x: number, _y: number, _fillColor: string, _fillColor2: string): void {
-            //Stiel
+        function drawBienenstock(_x: number, _y: number, _fillColor: string, _strokeColor: string): void {
+            // Dach
             crc2.beginPath();
-            crc2.fillRect(_x, _y, 10, 90);
-            crc2.closePath();
             crc2.fillStyle = _fillColor;
-            crc2.fill();
-            //Rechteck
-            crc2.beginPath();
-            crc2.fillRect(_x - 30, _y - 60, 80, 70);
+            crc2.arc(_x, _y, 50, 0, Math.PI, true);
             crc2.closePath();
+            crc2.fill();
+            //Körper
+            crc2.beginPath();
             crc2.fillStyle = _fillColor;
-            crc2.fill();
-            //Kreis
-            crc2.beginPath();
-            crc2.arc(_x + 8, _y - 27, 16, 0 * Math.PI, 2 * Math.PI);
+            crc2.fillRect(_x - 50, _y, 100, 60);
             crc2.closePath();
-            crc2.fillStyle = _fillColor2;
+            crc2.fill();
+            //Eingang
+            crc2.beginPath();
+            crc2.strokeStyle = "#201500";
+            crc2.fillStyle = "#FFD39B";
+            crc2.arc(550, 820, 30, 0, 2 * Math.PI);
+            crc2.closePath();
+            crc2.stroke();
             crc2.fill();
         }
 
-        function drawBee(x: number, y: number, _fillColor1: string, _fillColor2: string): void {
+        function drawBiene(_x: number, _y: number, _fillColor: string, _strokeColor: string): void {
+
+            //Flügel
             crc2.beginPath();
-            crc2.fillRect(x, y, 3, 9);
+            crc2.fillStyle = "#B9FFFF";
+            crc2.arc(_x + 1, _y - 8, 5, 0, 2 * Math.PI);
             crc2.closePath();
-            crc2.fillStyle = _fillColor1;
             crc2.fill();
             crc2.beginPath();
-            crc2.fillRect(x - 3, y, 3, 9);
+            crc2.fillStyle = "#B9FFFF";
+            crc2.arc(_x + 9, _y + 3, 5, 0, 2 * Math.PI);
             crc2.closePath();
-            crc2.fillStyle = _fillColor1;
             crc2.fill();
+
+            // Kopf
             crc2.beginPath();
-            crc2.arc(x - 7, y + 5, 3, 0 * Math.PI, 2 * Math.PI);
-            crc2.closePath();
-            crc2.fillStyle = _fillColor2;
+            crc2.fillStyle = "#000000";
+            crc2.arc(_x - 1, _y + 2, 5, 0, 2 * Math.PI);
             crc2.fill();
+
+            // Auge
             crc2.beginPath();
-            crc2.fillRect(x + 6, y, 3, 9);
-            crc2.closePath();
-            crc2.fillStyle = _fillColor1;
+            crc2.fillStyle = "white";
+            crc2.arc(_x - 2, _y + 1, 1, 0, 2 * Math.PI);
             crc2.fill();
+
+            // Körper
+            crc2.fillStyle = "#000000";
+            crc2.fillRect(_x, _y, 3, 10);
+
+            crc2.fillStyle = "yellow";
+            crc2.fillRect(_x + 3, _y, 3, 10);
+
+            crc2.fillStyle = "#000000";
+            crc2.fillRect(_x + 6, _y, 3, 10);
+
+            crc2.fillStyle = "yellow";
+            crc2.fillRect(_x + 9, _y, 3, 10);
+
+            crc2.fillStyle = "#000000";
+            crc2.fillRect(_x + 12, _y, 3, 10);
+
+            //Fühler
             crc2.beginPath();
-            crc2.fillRect(x + 3, y, 3, 9);
+            crc2.moveTo(_x - 2, _y);
+            crc2.strokeStyle = "#000000";
+            crc2.lineTo(_x - 6, _y - 6);
             crc2.closePath();
-            crc2.fillStyle = _fillColor2;
-            crc2.fill();
+            crc2.stroke();
+
+            //Fühler 2
+            crc2.beginPath();
+            crc2.moveTo(_x - 2, _y);
+            crc2.strokeStyle = "#000000";
+            crc2.lineTo(_x - 1, _y - 6);
+            crc2.closePath();
+            crc2.stroke();
+
+            //Stachel
+            crc2.beginPath();
+            crc2.moveTo(_x + 15, _y + 5);
+            crc2.strokeStyle = "#000000";
+            crc2.lineTo(_x + 18, _y + 5);
+            crc2.closePath();
+            crc2.stroke();
         }
     }
 }
